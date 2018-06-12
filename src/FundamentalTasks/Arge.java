99f578e26317d8da5a9c130632c47c70a5dfb5d8
Vytautas Zvirblis <vytas.zvirblis@gmail.com>
2018-06-12 22:16:52 +0200

    .ignore file added

--- src/FundamentalTasks/Arge.java
+++ src/FundamentalTasks/Arge.java
@@ -5,14 +5,15 @@ package FundamentalTasks;
  */
 public class Arge {
     //TODO
-    public static int nbYear (int p0, double percent, int aug, int p) {
+    public static int nbYear(int p0, double percent, int aug, int p) {
         int n = 0;
         while (p0 <= p) {
-          int atTheEndOfTheYear = (int) (p0+(p0*(percent/100))+aug);
-          p0=atTheEndOfTheYear;
-          n++;
-          if (p0>p)
-              break;
+            int atTheEndOfTheYear = (int) (p0 + (p0 * (percent / 100)) + aug);
+            p0 = atTheEndOfTheYear;
+            n++;
+            if (p0 > p)
+                break;
         }
         return n;
-    }}
+    }
+}
